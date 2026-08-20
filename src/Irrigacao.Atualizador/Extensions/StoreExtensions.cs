e0dabbe46f18415c5b18b65ec90f21a1a7a9d81c
Dev-Raiam <raiamdesenvolvedor@gmail.com>
2026-08-20 17:07:49 -0300

    Update

---
+++ src/Irrigacao.Atualizador/Extensions/StoreExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System.Runtime.InteropServices;
+using Toolbox.Industrial.Core.Data;
+using Toolbox.Industrial.Core.Extensions;
+
+namespace Irrigacao.Atualizador.Extensions
+{
+    // Mudar para o Toolbox.Industrial.Core
+    public static class StoreExtensions
+    {
+        public static async Task<UpdateRequest> ObterCredenciais(this IEntityStore store)
+        {
+            var contaId = await store.ObterConfiguracao<Guid>(Entity.Keys.ContaId);
+            var painelId = await store.ObterConfiguracao<Guid>(Entity.Keys.PainelId);
+            var controladorId = await store.ObterConfiguracao<Guid>(Entity.Keys.ControladorId);
+            var versaoAtual = await store.ObterConfiguracao<string>(Entity.Keys.VersaoAtual);
+
+            return new UpdateRequest(
+                contaId,
+                painelId,
+                controladorId,
+                null,
+                versaoAtual ?? "",
+                null,
+                (int)RuntimeInformation.OSArchitecture
+            );
+        }
+    }
+}
--- src/Irrigacao.Atualizador/Program.cs
+++ src/Irrigacao.Atualizador/Program.cs
@@ -12,9 +12,7 @@ if (args.Length > 0 && (args[0] == "--version" || args[0] == "-v"))
     return;
 }
 
-Log.Logger = new LoggerConfiguration()
-    .WriteTo.Console()
-    .CreateBootstrapLogger();
+Log.Logger = new LoggerConfiguration().WriteTo.Console().CreateBootstrapLogger();
 
 try
 {
@@ -24,18 +22,7 @@ try
 
     builder.Services.AddHttpClient();
 
-    builder.Services.AddSingleton(
-        new UpdateInstallationConfig(
-            BinaryName: "irrigacao",
-            ServiceName: "irrigacao",
-            BinaryDirectory: "/opt/edge-plc",
-            UpdateDirectory: "/var/tmp/edge-plc",
-            BackupPath: "/var/backups/edge-plc",
-            Url: "/automacao/v1/integracoes/2eb57304-1df3-4883-8f81-29b3e9426f6c/atualizacao-disponivel",
-            UrlConfirm : "/automacao/v1/integracoes/2eb57304-1df3-4883-8f81-29b3e9426f6c/confirmar-download-atualizacao"
-        )
-    );
-
+    builder.Services.AddSingleton<IUpdateInstaller, UpdateInstaller>();
     builder.Services.AddHostedService<Updater>();
 
     await Application.RunAsync(
@@ -46,7 +33,7 @@ try
 }
 catch (Exception ex)
 {
-    Log.Fatal("A Aplica��o falhou ao iniciar. {ex}", ex);
+    Log.Fatal("A Aplicação falhou ao iniciar. {ex}", ex);
 }
 finally
 {
